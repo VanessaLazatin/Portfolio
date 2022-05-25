fc26e9d422de96ec62b16873772ddadd6bcd0669
VanessaLazatin <lazatinvanessa@gmail.com>
2022-05-25 10:43:25 +0200

    Portfolio

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import REACTDOM from "react-dom/client";
+import Scroll from "./scroll"
+import "./App.css";
+import mypicture from "./image/vanessaSmall.jpg";
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <img src={mypicture} className="mypicture" alt="mypicture" />
+        <div id="toptitle" className="title">
+          Hi{" "}
+        </div> 
+        <div id="sndtitle" className="title">
+          I'm Vanessa
+        </div>
+        <div id="rdtitle" className="title">
+          React Developer 
+         <Scroll/> 
+        </div> 
+        <div id="myline"></div>
+        
+      </div>
+
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Hi I'm Vanessa
+      </a>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/scroll.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import "./style.css";
+function Scroll() {
+  return (
+    <div><div id="scroll-wrapper">
+    <div id="scroll-wrapper-inner">
+    <div id="scroll-title">
+      Scroll
+    </div>
+   <div id="scroll-down">
+  </div>
+    </div>
+    </div></div>
+  )
+}
+
+export default Scroll
